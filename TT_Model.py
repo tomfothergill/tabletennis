6463db1861a5ae2e19e460dd96ff978232199325
tomfothergill <74731740+tomfothergill@users.noreply.github.com>
2021-06-16 10:03:49 +0100

    Update TT_Model.py

--- TT_Model.py
+++ TT_Model.py
@@ -79,7 +79,7 @@ col2.subheader("Total Profit: " + str(total_profit_loss) + " units")
 #st.text(total_profit_loss)
 col2.subheader("Total bets: " + str(number_of_bets))
 col2.subheader("P/L: " + str(p_l_perc) + "%")
-col2.subheader("Rough Profit (TF):" + "£" + str(profit_tf))
+col2.subheader("Rough Profit (TF): " + "£" + str(profit_tf))
 
 if st.button("Show all bets in range:"):
     source
